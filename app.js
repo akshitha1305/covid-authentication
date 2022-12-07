4c84d8356aea8d258897e0e47c9ec361296c0a8b
akshitha13 <akshithamerugu321@gmail.com>
2022-12-07 13:19:18 +0000

    display responses

--- app.js
+++ app.js
@@ -25,31 +25,58 @@ const initializeDbAndServer = async () => {
 };
 
 initializeDbAndServer();
-
-//User Register
-
+//register User
 app.post("/users/", async (request, response) => {
   const { username, name, password, gender, location } = request.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  console.log(hashedPassword);
-  const getUserDetails = `
-  SELECT * FROM user WHERE username = '${username}';
-  `;
-  const dbUser = await db.get(getUserDetails);
+  const hashedPassword = await bcrypt.hash(request.body.password, 10);
+  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
+  const dbUser = await db.get(selectUserQuery);
   if (dbUser === undefined) {
-    const createUser = `
-    INSERT INTO user(username, name, password, gender, location)
-    VALUES(
-        '${username}',
-        '${name}',
-        '${hashedPassword}',
-        '${gender}',
-        '${location}'
-    );`;
-    const dbResponse = await db.run(createUser);
-    response.send(dbResponse);
+    const createUserQuery = `
+      INSERT INTO 
+        user (username, name, password, gender, location) 
+      VALUES 
+        (
+          '${username}', 
+          '${name}',
+          '${hashedPassword}', 
+          '${gender}',
+          '${location}'
+        )`;
+    const dbResponse = await db.run(createUserQuery);
+    const newUserId = dbResponse.lastID;
+    response.send(`Created new user with ${newUserId}`);
   } else {
     response.status = 400;
     response.send("User already exists");
+  }
+});
+
+//Login user
+app.post("/login/", async (request, response) => {
+  const { username, password } = request.body;
+  const getUserDetails = `
+    SELECT * 
+    FROM user
+    WHERE username = '${username}';
+    `;
+  const dbUser = await db.get(getUserDetails);
+  console.log(username);
+  console.log(dbUser);
+  if (dbUser === undefined) {
+    response.status(400);
+    response.send("Invalid user");
+  } else {
+    const isPasswordMatched = bcrypt.compare(password, dbUser.password);
+    if (isPasswordMatched === true) {
+      const { username: username } = payload;
+      const jwtToken = jwt.sign(payload, "asdfghj");
+      response.send({
+        jwtToken,
+      });
+    } else {
+      response.status(400);
+      response.send("Invalid password");
+    }
   }
 });
